01c3f31d0235ec741647ecbe6ccad13bd562c440
Charles-Henri St-Mars <ch.stmars@gmail.com>
2022-03-02 16:43:48 +0100

    Correction de l'adresse du fichier JSON

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -11,9 +11,17 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const h3 = document.createElement( 'h3' );
+        h3.textContent = `${city}, ${country}`;
+        const tag = document.createElement( 'p' );
+        tag.textContent = tagline;
+        const prix = document.createElement( 'p' );
+        prix.textContent = `${price}€/jour`;
         article.appendChild(img);
         article.appendChild(h2);
-        debugger;
+        article.appendChild(h3);
+        article.appendChild(tag);
+        article.appendChild(prix);
         return (article);
     }
     return { name, picture, getUserCardDOM }
