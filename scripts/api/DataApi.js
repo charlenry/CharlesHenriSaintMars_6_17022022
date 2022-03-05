1d2521c0b7f527a111563357c098665e0451478c
Charles-Henri St-Mars <ch.stmars@gmail.com>
2022-03-05 18:34:10 +0100

    Restructuration de la base de code en vue d'utiliser le factory pattern

--- scripts/api/DataApi.js
+++ scripts/api/DataApi.js
@@ -11,15 +11,29 @@ class DataApi {
 
   async getPhotographers() {
     return fetch(this._url)
-        .then(res => res.json())
-        .then(res => res.photographers)
-        .catch(err => console.log('an error occurs', err));
+      .then(response => {
+        if(response.ok) return response.json();
+        else throw new Error(response.statusText);
+      })
+      .then(data => {
+        return (data.photographers);
+      })
+      .catch(error => {
+        console.log(`HTTP Error: ${error.message}`);
+      });
   }
 
   async getMedia() {
     return fetch(this._url)
-      .then(res => res.json())
-      .then(res => res.media)
-      .catch(err => console.log('an error occurs', err));
+    .then(response => {
+      if(response.ok) return response.json();
+      else throw new Error(response.statusText);
+    })
+    .then(data => {
+      return (data.media);
+    })
+    .catch(error => {
+      console.log(`HTTP Error: ${error.message}`);
+    });
   }
 }
