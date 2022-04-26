a68e371a1c22b0df7b41c7b40843be0fc80cf89a
Charles-Henri St-Mars <ch.stmars@gmail.com>
2022-04-27 00:39:23 +0200

    Ajout du favicon et quelques corrections de bugs

--- assets/scripts/templates/PhotographerData.js
+++ assets/scripts/templates/PhotographerData.js
@@ -18,9 +18,22 @@ class PhotographerData {
     const $h1 = $("<h1>").html(`${this._photographer.name}`);
     const $h2 = $("<h2>").html(`${this._photographer.city}, ${this._photographer.country}`);
     const $p = $("<p>").html(`${this._photographer.tagline}`);
-    $h1.attr('tabindex', '1');
-    $h2.attr('tabindex', '1');
-    $p.attr('tabindex', '1');
+    
+    $h1.attr({
+      'aria-label': `Mon nom : ${this._photographer.name}`,
+      tabindex: '1'
+    });
+
+    $h2.attr({
+      'aria-label': `Ma localité : ${this._photographer.city}, ${this._photographer.country}`,
+      tabindex: '1'
+    });
+
+    $p.attr({
+      'aria-label': `Mon slogan : ${this._photographer.tagline}`,
+      tabindex: '1'
+    });
+
     return [$h1, $h2, $p];
   }
 }
